a30a420bd88ba2994615da2f4f2fae47b6366eb3
Lesli <lpatino0994@gmail.com>
2022-09-04 04:15:46 -0500

    added local storage functionality and fixed bug

--- script.js
+++ script.js
@@ -2,31 +2,53 @@
 var lat ='';
 var lon ='';
 var result='';
+flag = false;
+
+//query selectors
 var searchBtnEl = document.getElementById('searchBtn');
 var tempEl = document.getElementById('temp');
 var windEl = document.getElementById('wind');
 var humidityEl = document.getElementById('humidity');
 var uvEl = document.getElementById('uv');
 var cityHeaderEl = document.getElementById('cityHeader');
-var weatherBoardEl = document.getElementById('weatherBoard');
+var IconEl = document.getElementById('cityIcon');
+
+var descriptionEL = document.getElementById('cd1');
+var currentDateEl= document.getElementById('cwd');
+var currentWeatherEl= document.getElementById('cw1');
 
 
-searchBtnEl.onclick = function search(){
+
+
+
+searchBtnEl.onclick = function search(e){
+    e.preventDefault();
     start();
-    setCity();
     
 }
 
-function setCity(){
-    cityEl = {city: ''};
-    cityEl.city = document.getElementById('cityName').value;
-    console.log(cityEl.city)
-    return cityEl.city;
+function setCity(query){
+
+    if (flag){
+        console.log(query)
+        return query;
+    }else{
+        cityEl = {city: ''};
+        cityEl.city = document.getElementById('cityName').value;
+   
+     return cityEl.city;
+    }
 }
 
 // on click function. Grabs the lon and lat of the city searched
  async function start(){
      result = await setCity();
+     flag=false;
+    console.log(result)
+     window.localStorage.setItem(`${result}`,`${result}`);
+
+
+
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${result}&appid=f4e9f8a1bbdb70ea1144dd0d671eb352`;
    fetch(requestUrl)
     .then(res=>res.json())
@@ -42,24 +64,120 @@ function setCity(){
 
 
 
-
 function getWeather(lat, lon, result){
-    var requestWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=f4e9f8a1bbdb70ea1144dd0d671eb352`;
+    var requestWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=f4e9f8a1bbdb70ea1144dd0d671eb352`;
 
     fetch(requestWeatherUrl)
     .then(res=>res.json())
     .then(data=>{
+
+        console.log(data);
+
+        let placeholder=`${data.current.weather[0].icon}`
+        
+        //icon url
+        const icon = `http://openweathermap.org/img/wn/${placeholder}@2x.png`;
+       
+        let iconImg= document.createElement('img');
+        iconImg.src=icon;
+        IconEl.src=icon;
+
+
         //edits the innherHTML of front end elements to display weather conditions of searched city
-        cityHeaderEl.innerHTML=``;
-        tempEl.innerHTML  = ``;
-        humidityEl.innerHTML  = ``;
-        windEl.innerHTML  = ``;
-        uvEl.innerHTML  = ``;
+        descriptionEL.innerHTML  = `${data.current.weather[0].description}`;
+        currentDateEl.innerHTML =`${data.current.dt}`;
+        currentWeatherEl.innerHTML =`Description: ${data.current.weather[0].main}`;
         cityHeaderEl.innerHTML=`${result}`;
         tempEl.innerHTML  = `Temp: ${data.current.temp}`;
         humidityEl.innerHTML  = `Humidity: ${data.current.humidity}`;
-        windEl.innerHTML  = `Wind: ${data.current.wind_speed}`;
+        windEl.innerHTML  = `Wind: ${data.current.wind_speed} MPH`;
         uvEl.innerHTML  = `UV Index: ${data.current.uvi}`;
-        // weatherDescriptionEl.innerHTML = `${data.current.weather[0].main}`
+       ////////////////////////////////////////////////////////////////////////
+
+        var dailyContainerEl = document.getElementById('dailyContainer');
+
+        //removes previous search's cards to avoid duplicates
+        function removeCards(){
+            let element=document.getElementById('dailyCards');
+            if(typeof(element) != 'undefined' && element !=null){
+                return;
+            } else{
+                // while(dailyItemEl.hasChildNodes(){
+                //     dailyItemEl.removeChild(dailyItemEl.firstChild);
+                // }
+                while(dailyContainerEl.hasChildNodes()){
+                    dailyContainerEl.removeChild(dailyContainerEl.firstChild);
+                }
+            }
+            
+        }
+        removeCards();
+
+        for(i=0; i<5;i++){
+            //grab html element
+           
+            placeholder=`${data.daily[i].weather[0].icon}`
+
+            
+            //create elements
+            var dailyCards= document.createElement('div');
+            dailyCards.setAttribute("class", "forecastItems col-3");
+            dailyContainerEl.appendChild(dailyCards);
+          
+            //create icon element
+            var dailyIcon= document.createElement("img");
+            dailyIcon.src=icon;
+            dailyCards.appendChild(dailyIcon);
+            
+            var h4El =document.createElement('h4');
+            h4El.textContent=`${data.daily[i].dt}`;
+            dailyCards.appendChild(h4El);
+
+            var dailyItemEl=document.createElement('ul');
+            dailyCards.appendChild(dailyItemEl);
+
+            var li1 = document.createElement('li');
+            var li2 = document.createElement('li');
+            var li3 = document.createElement('li');
+            var li4 = document.createElement('li');
+
+            //add text
+            li1.textContent=`Temp: ${data.daily[i].temp.day}`
+            li2.textContent=`Wind: ${data.daily[i].wind_speed} MPH`
+            li3.textContent=`Humidity: ${data.daily[i].humidity}`
+
+            dailyItemEl.appendChild(li1);
+            dailyItemEl.appendChild(li2);
+            dailyItemEl.appendChild(li3);
+            dailyItemEl.appendChild(li4);
+        }
+
+        
+        
+    })
+
+    renderHistory(result);
+   
+}
+
+function renderHistory(ok){
+    let newObject = window.localStorage.getItem(`${ok}`);
+  
+
+    var searchListEl =document.getElementById('searchList');
+    var item=document.createElement('li');
+    item.setAttribute("id", `${ok}`);
+    item.textContent=`${newObject}`;
+    searchListEl.appendChild(item);
+    
+    searchListEl.addEventListener("click", function searchAgain(e){
+        e.stopEventPropagation;
+        var target= e.target;
+        var targetContent= target.innerHTML;
+        console.log(targetContent)
+        start();
+        flag = true;
+        setCity(targetContent);
+
     })
 }
